80905e3b12e6e6ce0c8e9a4e0d524262320227d2
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-02-20 14:37:03 +0100

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1 className="text-lg m-4 py-4">About page</h1>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
-import './globals.css'
+import "./globals.css";
+import { Montserrat } from "@next/font/google";
+
+const monstserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-monstserrat"
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`${monstserrat.className}`}>{children}</body>
     </html>
-  )
+  );
 }
