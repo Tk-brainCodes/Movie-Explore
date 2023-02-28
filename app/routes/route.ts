ef9e5f70523c2283382508f41bce4e6dc130606e
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-02-28 23:38:47 +0100

    fix: remove icon data

--- app/routes/route.ts
+++ app/routes/route.ts
@@ -1,27 +1,14 @@
-import {
-  IconHome,
-  IconBrandSafari,
-  IconClock,
-  IconAlarm,
-  IconBookmark,
-  IconStar,
-  IconPlayerPlay,
-} from "@tabler/icons-react";
-
-
-
 export const sidebar_routes = [
   {
     name: "Menu",
     id: 1,
     paths: [
-      { title: "Home", url: "/", id: 1, icon: IconHome },
-      { title: "Discover", url: "/discover", id: 2, icon: IconBrandSafari },
+      { title: "Home", url: "/", id: 1 },
+      { title: "Discover", url: "/discover", id: 2 },
       {
         title: "Coming Soon",
         url: "/coming-soon",
         id: 3,
-        icon: IconAlarm,
       },
     ],
   },
@@ -29,10 +16,10 @@ export const sidebar_routes = [
     name: "Library",
     id: 2,
     paths: [
-      { title: "Recent", url: "/recent", id: 1, icon: IconClock },
-      { title: "Bookmarked", url: "/bookmarked", id: 2, icon: IconBookmark },
-      { title: "Top Rated", url: "/top-rated", id: 3, icon: IconStar },
-      { title: "Similar", url: "/similar", id: 4, icon: IconPlayerPlay },
+      { title: "Recent", url: "/recent", id: 1 },
+      { title: "Bookmarked", url: "/bookmarked", id: 2 },
+      { title: "Top Rated", url: "/top-rated", id: 3 },
+      { title: "Similar", url: "/similar", id: 4 },
     ],
   },
 ];
