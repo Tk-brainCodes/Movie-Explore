bbd53e1b3c9b1c0968ef543d17dc567923e2ab67
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-07 13:54:33 +0100

    feat: topnav component

--- app/components/Topnav.tsx
+++ app/components/Topnav.tsx
@@ -1,5 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoCamera } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
 export default function TopNav() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className='w-full h-20 border-b-2 px-4 py-6 flex items-center justify-between fixed bg-white z-10'>
+      <div className='font-black mb-6 flex gap-2 h-4 mt-5'>
+        <FontAwesomeIcon
+          icon={faVideoCamera}
+          style={{ fontSize: 20, color: "black" }}
+        />
+        MovieExpore
+      </div>
+      <div className="w-auto px-4 py-4">
+        <ul className="flex gap-5 font-semibold text-sm text-slate-500">
+          <Link href="/movies">Movie</Link>
+          <Link href="/tvshows">Tv Shows</Link>
+          <Link href="/search">Find Movies</Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
