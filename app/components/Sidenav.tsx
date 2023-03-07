f83431cf72069cefec372837e30e1d0001349dd5
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-07 13:52:01 +0100

    feat: removed icon, replaced button with link

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -1,34 +1,32 @@
+"use client";
 import { sidebar_routes } from "../routes/route";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVideoCamera } from "@fortawesome/free-solid-svg-icons";
-import { IconHome } from "@tabler/icons-react";
-
+import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Sidenav() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
-    <div className='w-64 h-screen border border-grey-600 px-4 py-4 fixed'>
-      <div className='font-black mb-6 flex gap-2 h-4'>
-        <FontAwesomeIcon
-          icon={faVideoCamera}
-          style={{ fontSize: 20, color: "black" }}
-        />
-        MovieDB
-      </div>
-      <ul>
+    <div className='w-64 bg-white h-screen border-r-2 border-grey-600 px-4 py-4 fixed'>
+      <ul className='mt-[5em]'>
         {sidebar_routes.map((route) => (
           <div key={route.id} className=' grid grid-cols-1'>
-            <br />
-            <li className='font-black mb-4'>{route.name}</li>
+            <li className='font-black mt-4'>{route.name}</li>
             <li className='flex flex-col gap-3 text-sm'>
               {route.paths.map((paths) => (
-                <Link
+                <button
                   key={paths.id}
-                  className='flex mt-2 items-center justify-start h-6'
-                  href={paths.url}
+                  className={`flex text-sm mt-2 items-center justify-start h-6 ${
+                    pathname === paths.url
+                      ? "text-orange-400 font-medium"
+                      : "text-slate-400"
+                  }`}
+                  onClick={() => router.push(`${paths.url}`)}
                 >
                   {paths.title}
-                </Link>
+                </button>
               ))}
             </li>
           </div>
