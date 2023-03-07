215787b2c1d0d97e3838910ac199787704c592af
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-07 13:47:41 +0100

    feat: add movieContainer and showingTheatre

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,71 @@
+"use client";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import Movies from "./movies/Movies";
+import MovieContainer from "./components/MovieCard";
+import ShowingInTheater from "./components/ShowingTheatres";
 
-interface MovieDataProp {
-  adult?: boolean;
-  backdrop_path?: string;
-  genre_ids?: Array<string>;
-  original_language?: string;
-  original_title?: string;
-  overview?: string;
-  popularity?: number;
-  poster_path?: string;
-  release_date?: string;
-  title?: string;
-  video?: boolean;
-  vote_average?: number;
-  vote_count?: number;
-  id?: number;
-}
+const getData = async (fn: any, url: string, config: any) => {
+  try {
+    const resp = await axios.get(url, config);
+    const data = await resp?.data;
+    fn(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default async function Home() {
-   const res = await axios.get(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState<any[]>([]);
+  const [trendingMovies, setTrendingMovies] = useState<any[]>([]);
+  const [nowShowing, setNowShowing] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const myKey = process.env.API_KEY;
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const getPopularMovies = getData(
+    setPopularMovies,
+    `https://api.themoviedb.org/3/movie/popular?api_key=${myKey}`,
+    config
   );
-  const popularMovies = await res.data;
 
+  const getTrendingMovies = getData(
+    setTrendingMovies,
+    `https://api.themoviedb.org/3/trending/all/day?api_key=${myKey}`,
+    config
+  );
+
+  const getMoviesInTheatres = getData(
+    setNowShowing,
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${myKey}&language=en-US&page=${currentPage}`,
+    config
+  );
+
+  useEffect(() => {
+    getPopularMovies;
+    getTrendingMovies;
+    getMoviesInTheatres;
+    localStorage.setItem("nowShowing", JSON.stringify(nowShowing));
+    localStorage.setItem("popular", JSON.stringify(popularMovies));
+    localStorage.setItem("trending", JSON.stringify(trendingMovies));
+  }, [nowShowing, popularMovies, trendingMovies]);
 
   return (
     <main>
-      <h1 className='text-black font-bold text-lg mb-1'>POPULAR MOVIES</h1>
-      <div className='grid gap-16 grid-cols-fluid'>
-        {popularMovies?.results?.map((movie: MovieDataProp) => (
-          <Movies
-            title={movie?.title as string}
-            movieId={movie?.id as number}
-            poster_path={movie?.poster_path as string}
-            release_date={movie?.release_date as string}
-            key={movie?.id}
-          />
-        ))}
-      </div>
+      <ShowingInTheater
+        movies={nowShowing}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <MovieContainer text='Popular Movies' movie={popularMovies} />
+      <MovieContainer text='Trending Movies' movie={trendingMovies} />
     </main>
   );
-}
+};
 
-
+export default Home;
