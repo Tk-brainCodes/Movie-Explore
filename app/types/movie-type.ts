e8ed85ad0c023855c44a92da68d0ebb238914c17
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-07 13:59:12 +0100

    feat(type): movie components types

---
+++ app/types/movie-type.ts
@@ -0,0 +1,23 @@
+export interface MovieDataProp {
+  adult?: boolean;
+  backdrop_path?: string;
+  genre_ids?: Array<string>;
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  release_date?: string;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+  id?: number;
+}
+
+export interface MovieCardProps {
+  title: string;
+  movieId: number;
+  poster_path: string;
+  release_date: string;
+}
