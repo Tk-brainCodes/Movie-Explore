66749c45a6e3348b12e79ea43478049c27e95937
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-11 11:56:50 +0100

    chore: remove fontawesom configuration and declaration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import "./globals.css";
 import { Montserrat } from "@next/font/google";
 import Sidenav from "./components/Sidenav";
 import TopNav from "./components/Topnav";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-
-config.autoAddCss = false;
 
 const monstserrat = Montserrat({
   weight: ["400", "700"],
@@ -24,16 +20,17 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <link
+        rel='stylesheet'
+        href='https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css'
+      ></link>
       <head />
       <body className={`${monstserrat.className} w-full overflow-x-hidden`}>
-        <TopNav/>
-        <div className="grid">
+        <TopNav />
+        <div className='grid'>
           <Sidenav />
-          <div className="px-8 py-4 mt-[5em] ml-[15em]">
-            {children}
-          </div>
+          <div className='px-8 py-4 mt-[5em] ml-[15em]'>{children}</div>
         </div>
-
       </body>
     </html>
   );
