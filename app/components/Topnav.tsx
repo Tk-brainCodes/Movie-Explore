9599ea3e82fa10edfdd306952b5565020d8734ff
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-11 12:04:07 +0100

    chore: remove fontawesom, add mui-icons

--- app/components/Topnav.tsx
+++ app/components/Topnav.tsx
@@ -1,22 +1,19 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVideoCamera } from "@fortawesome/free-solid-svg-icons";
+"use client"
+import SlideshowOutlinedIcon from "@mui/icons-material/SlideshowOutlined";
 import Link from "next/link";
 
 export default function TopNav() {
   return (
     <div className='w-full h-20 border-b-2 px-4 py-6 flex items-center justify-between fixed bg-white z-10'>
       <div className='font-black mb-6 flex gap-2 h-4 mt-5'>
-        <FontAwesomeIcon
-          icon={faVideoCamera}
-          style={{ fontSize: 20, color: "black" }}
-        />
+        <SlideshowOutlinedIcon />
         MovieExpore
       </div>
-      <div className="w-auto px-4 py-4">
-        <ul className="flex gap-5 font-semibold text-sm text-slate-500">
-          <Link href="/movies">Movie</Link>
-          <Link href="/tvshows">Tv Shows</Link>
-          <Link href="/search">Find Movies</Link>
+      <div className='w-auto px-4 py-4'>
+        <ul className='flex gap-5 font-semibold text-sm text-slate-500'>
+          <Link href='/movies'>Movie</Link>
+          <Link href='/tvshows'>Tv Shows</Link>
+          <Link href='/search'>Find Movies</Link>
         </ul>
       </div>
     </div>
