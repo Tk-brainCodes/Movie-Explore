e46261f39414befe89fc4965a70c6da8b10d694e
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-11 12:09:08 +0100

    feat: add icon for sidenav menu content

--- app/routes/route.ts
+++ app/routes/route.ts
@@ -1,14 +1,28 @@
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ExploreRoundedIcon from "@mui/icons-material/ExploreRounded";
+import AccessAlarmRoundedIcon from "@mui/icons-material/AccessAlarmRounded";
+import ScheduleRoundedIcon from "@mui/icons-material/ScheduleRounded";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+
 export const sidebar_routes = [
   {
     name: "Menu",
     id: 1,
     paths: [
-      { title: "Home", url: "/", id: 1 },
-      { title: "Discover", url: "/discover", id: 2 },
+      { title: "Home", url: "/", id: 1, Icon: HomeRoundedIcon },
+      {
+        title: "Discover",
+        url: "/discover",
+        id: 2,
+        Icon: ExploreRoundedIcon,
+      },
       {
         title: "Coming Soon",
         url: "/coming-soon",
         id: 3,
+        Icon: AccessAlarmRoundedIcon,
       },
     ],
   },
@@ -16,10 +30,25 @@ export const sidebar_routes = [
     name: "Library",
     id: 2,
     paths: [
-      { title: "Recent", url: "/recent", id: 1 },
-      { title: "Bookmarked", url: "/bookmarked", id: 2 },
-      { title: "Top Rated", url: "/top-rated", id: 3 },
-      { title: "Similar", url: "/similar", id: 4 },
+      { title: "Recent", url: "/recent", id: 1, Icon: ScheduleRoundedIcon },
+      {
+        title: "Bookmarked",
+        url: "/bookmarked",
+        id: 2,
+        Icon: BookmarkBorderOutlinedIcon,
+      },
+      {
+        title: "Top Rated",
+        url: "/top-rated",
+        id: 3,
+        Icon: StarBorderOutlinedIcon,
+      },
+      {
+        title: "Similar",
+        url: "/similar",
+        id: 4,
+        Icon: CategoryOutlinedIcon,
+      },
     ],
   },
 ];
