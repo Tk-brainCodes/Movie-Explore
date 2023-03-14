8beb4b7d8ab99a3dc846f4577fa9bbc85652c909
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-14 20:16:39 +0100

    feat: add fallback component for no movies

--- app/components/CustomMovieCard.tsx
+++ app/components/CustomMovieCard.tsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import Movies from "../movies/Movies";
 import { MovieDataProp } from "../types/movie-type";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import VideocamOffOutlinedIcon from "@mui/icons-material/VideocamOffOutlined";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export default function CustomMovieCard({
   movies,
@@ -10,11 +13,11 @@ export default function CustomMovieCard({
   currentPage,
   title,
 }: any) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<Boolean>(false);
 
   useEffect(() => {
     if (movies?.results?.length === 0) {
-      setLoading(!loading);
+      setLoading(true);
     } else {
       setLoading(false);
     }
@@ -42,18 +45,30 @@ export default function CustomMovieCard({
       </h1>
 
       <div className='flex px-6 py-4 snap-mandatory snap-x scroll-pr-6 touch-auto scroll-smooth flex-row overflow-x-auto space-x-8 no-scrollbar h-[400px] w-[940px]'>
-        {movies?.results?.map((movie: MovieDataProp) => (
-          <div className='snap-center flex-shrink-0'>
-            <Movies
-              title={movie?.title as string}
-              movieId={movie?.id as number}
-              poster_path={movie?.poster_path as string}
-              backdrop_path={movie?.backdrop_path as string}
-              release_date={movie?.release_date as string}
-              key={movie?.id}
-            />
-          </div>
-        ))}
+        <>
+          {movies?.results?.length === 0 ? (
+            <div className='grid'>
+              <h2 className='text-slate-300 text-2xl font-normal block'>
+                <VideocamOffOutlinedIcon className='w-[100px] h-[100px] ml-[40px]' />
+                <br />
+                No {title}
+              </h2>
+            </div>
+          ) : (
+            movies?.results?.map((movie: MovieDataProp) => (
+              <div className='snap-center flex-shrink-0'>
+                <Movies
+                  title={movie?.title as string}
+                  movieId={movie?.id as number}
+                  poster_path={movie?.poster_path as string}
+                  backdrop_path={movie?.backdrop_path as string}
+                  release_date={movie?.release_date as string}
+                  key={movie?.id}
+                />
+              </div>
+            )) || <Skeleton count={10} height={400} />
+          )}
+        </>
       </div>
     </>
   );
