99f6698c9b4f706cffb1b308caed0891f7cb3ca5
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-03-14 20:14:39 +0100

    feat: add reduc provider and themeProvider contetxt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+"use client";
 import "./globals.css";
 import { Montserrat } from "@next/font/google";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import Sidenav from "./components/Sidenav";
 import TopNav from "./components/Topnav";
+import { ThemeContextProvider } from "./context/theme";
 
 const monstserrat = Montserrat({
   weight: ["400", "700"],
@@ -26,11 +30,15 @@ export default function RootLayout({
       ></link>
       <head />
       <body className={`${monstserrat.className} w-full overflow-x-hidden`}>
-        <TopNav />
-        <div className='grid'>
-          <Sidenav />
-          <div className='px-8 py-4 mt-[5em] ml-[15em]'>{children}</div>
-        </div>
+        <Provider store={store}>
+          <ThemeContextProvider>
+            <TopNav />
+            <div className='grid'>
+              <Sidenav />
+              <div className='px-8 py-4 mt-[5em] ml-[15em]'>{children}</div>
+            </div>
+          </ThemeContextProvider>
+        </Provider>
       </body>
     </html>
   );
