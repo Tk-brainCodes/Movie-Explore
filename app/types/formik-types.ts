eb3777c3f3fa319b7a2ed8bf34503bc9f89795a2
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-05-01 13:40:53 +0100

    feat(type): formik form types

---
+++ app/types/formik-types.ts
@@ -0,0 +1,13 @@
+export interface FormValues {
+  email: string;
+  password: string;
+}
+
+export interface OtherProps {
+  message: string;
+}
+
+export interface MyFormProps {
+  initialEmail?: string;
+  message: string;
+}
