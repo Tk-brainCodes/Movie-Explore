b5d3b94e8993bfedf5f369184ab922812d9ca518
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-05-01 13:26:43 +0100

    refactor: remove logout button and move it to ~ TopNav Component

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -1,56 +1,116 @@
 "use client";
 import { sidebar_routes } from "../routes/route";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useContext, useState, useEffect } from "react";
+import { GlobalContext } from "../context/Globalcontext";
+import { motion } from "framer-motion";
+import no_image from "../../../../public/image/no_image.jpg";
+import "react-responsive-modal/styles.css";
 import Link from "next/link";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
 
 export default function Sidenav() {
-  const router = useRouter();
   const pathname = usePathname();
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { bookmarked, toggleSidebar, isSidebarOpen, sidebarRef } =
+    useContext(GlobalContext);
+  // const numItems = bookmarked?.length;
+
+  const sideVariants = {
+    closed: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: 1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    closed: {
+      opacity: 0,
+    },
+    open: { opacity: 1 },
+  };
+
   return (
-    <div className='w-64 bg-white h-screen border-r-2 border-grey-600 px-4 py-4 fixed'>
-      <ul className='mt-[5em]'>
-        {sidebar_routes.map((route) => (
-          <div key={route.id} className=' grid grid-cols-1'>
-            <li className='font-black mt-4'>{route.name}</li>
-            <li className='flex flex-col gap-3 text-sm'>
-              {route.paths.map(({ id, title, Icon, url }) => {
-                return (
-                  <button
-                    key={id}
-                    className={`flex text-sm mt-2 items-center text-center gap-3 justify-start hover:text-orange-400 transition ease-in-out h-6 ${
-                      pathname === url
-                        ? "text-orange-400 font-semibold"
-                        : "text-slate-400"
-                    }`}
-                    onClick={() => router.push(`${url}`)}
-                  >
-                    <span>{<Icon />}</span>
-                    {title}
-                  </button>
-                );
-              })}
-            </li>
-          </div>
-        ))}
-        <br />
-        <Link
-          className='flex gap-3 hover:text-orange-400 text-sm mb-6 mt-2 transition ease-in-out text-slate-400'
-          href='/settings'
+    <>
+      {isSidebarOpen && (
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: 256 }}
+          className={`w-64 top-0 left-0 bg-gray-900 z-10  h-screen border-r-2 border-slate-800 transition-transform border-grey-600 px-4 py-4 fixed ease-in-out duration-300 ${
+            isSidebarOpen ? "block" : "max-sm:hidden"
+          }`}
+          aria-label='Sidebar'
+          ref={sidebarRef}
         >
-          <SettingsOutlinedIcon />
-          Settings
-        </Link>
-        <Link
-          className='flex gap-3 hover:text-orange-400 transition ease-in-out text-sm bottom-0 text-slate-400'
-          href='/'
-        >
-          <LogoutOutlinedIcon />
-          Log out
-        </Link>
-      </ul>
-    </div>
+          <motion.ul
+            initial='closed'
+            animate='open'
+            variants={sideVariants}
+            className='mt-[5em]'
+          >
+            {sidebar_routes.map((route) => (
+              <div key={route.id} className=' grid grid-cols-1'>
+                <li className='text-white mt-4 mb-[10px]'>{route.name}</li>
+                <li className='flex flex-col gap-4 text-sm'>
+                  {route.paths.map(({ id, title, Icon, url }) => {
+                    return (
+                      <Link
+                        href={`${url}`}
+                        key={id}
+                        className={`flex text-sm mt-2 items-center text-center gap-3 justify-start hover:text-orange-400 transition ease-in-out h-6 ${
+                          pathname === url
+                            ? "text-orange-400 font-semibold"
+                            : "text-slate-400"
+                        }`}
+                      >
+                        <span>{<Icon />}</span>
+                        <motion.span
+                          whileHover={{ scale: 1.1 }}
+                          transition={{
+                            type: "spring",
+                            stiffness: 400,
+                            damping: 10,
+                          }}
+                          variants={itemVariants}
+                        >
+                          {title}
+                        </motion.span>
+                        {/* {title === "Bookmarked" ? (
+                      <>
+                        {title}
+                        {numItems ? (
+                          <span className='w-[25px] h-[25px] flex px-2 items-center justify-center -ml-[6px] -mt-[15px] relative bg-red-500 rounded-full text-white'>
+                            <p className='text-xs font-semibold'>
+                              {numItems ? numItems : ""}
+                            </p>
+                          </span>
+                        ) : (
+                          ""
+                        )}
+                      </>
+                    ) : (
+                      title
+                    )} */}
+                      </Link>
+                    );
+                  })}
+                </li>
+              </div>
+            ))}
+          </motion.ul>
+        </motion.div>
+      )}
+    </>
   );
 }
