ed8d94d11ef3487c85457641e5d8b3a7c1a1bccc
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-07-19 21:40:20 +0100

    feat: bookmark component

--- app/bookmarked/page.tsx
+++ app/bookmarked/page.tsx
@@ -1,61 +1,103 @@
-// "use client";
-// import { useState, useEffect, useContext } from "react";
+"use client";
+import { useState, useEffect, useContext } from "react";
 import { GlobalContext } from "../context/Globalcontext";
 import Image from "next/image";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 import Link from "next/link";
 import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
+import LoadingSpiner from "../components/LoadingSpinner";
 
-const Bookmarked = async () => {
-  // const [myBookmarked, setBookmarked] = useState([]);
+
+const Bookmarked = () => {
+  const [myBookmarked, setBookmarked] = useState([]);
+  const [localStorageBookMarks, setLocalStorageBookmarks] = useState([]);
   const imagePath = "https://image.tmdb.org/t/p/original";
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  // const { bookmarked, removeMovieFromBookmarked } = useContext(GlobalContext);
+  const {
+    bookmarked,
+    // @ts-ignore
+    removeMovieFromBookmarked,
+    // @ts-ignore
+    getBookmarksFromFirebaseDB,
+    // @ts-ignore
+    loading,
+    // @ts-ignore
+    notifySuccess,
+    // @ts-ignore
+    notifyError,
+  } = useContext(GlobalContext);
 
-  // const handleRemoveBookmarked = (id: number) => {
-  //   removeMovieFromBookmarked(id);
-  // };
 
-  // useEffect(() => {
-  //   setBookmarked(bookmarked);
-  // }, [myBookmarked, bookmarked]);
+  const handleRemoveBookmarked = (id: number) => {
+    try {
+      removeMovieFromBookmarked(id);
+      notifySuccess();
+    } catch (error: any) {
+      notifyError();
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    setBookmarked(bookmarked); 
+    getBookmarksFromFirebaseDB();       
+    const storedItem = JSON.parse(localStorage.getItem("myBookmarks") as string)
+    setLocalStorageBookmarks(storedItem)
+  }, [myBookmarked, localStorageBookMarks]);
+
+    console.log("bookmarks", bookmarked)
+
+    let checkIFBookmarkEmpty = myBookmarked.length === 0 ? localStorageBookMarks : myBookmarked;
+
 
   return (
     <div className='w-fit'>
-      <h1 className='font-semibold mb-4'>My Bookmarks</h1>
-      {/* <div className='grid grid-cols-4 gap-6'>
-        {myBookmarked.map((movie: any) => (
-          <div className='w-fit '>
-            <Link href={`movies/${movie.id}`}>
-              <Image
-                src={imagePath + movie.poster_path}
-                alt={`${movie.title}`}
-                className='h-[350px] w-[250px] rounded-md bg-stone-300 transition ease-in-out cursor-pointer hover:brightness-50'
-                loading='lazy'
-                width={500}
-                height={500}
-                blurDataURL={imagePath + movie.poster_path}
-                placeholder='blur'
-              />
-            </Link>
-            <div className='flex gap-2 relative -mt-[20em] float-right px-2'>
-              <button
-                title='bookmark movie'
-                className={`text-xs bg-white text-slate-500 px-3 py-3 hover:scale-110 transition ease-in-out rounded-full`}
-                onClick={() => handleRemoveBookmarked(movie.id)}
-              >
-                <BookmarkAddedIcon className='text-green-500' />
-              </button>
-            </div>
-            <h1 className='mt-3 text-slate-600 font-semibold tracking-tight'>
-              {movie.title}
-            </h1>
+      <h1 className='font-semibold mb-4 text-white'>My Bookmarks</h1>
+      <div>
+        {loading ? (
+          <LoadingSpiner text={"bookmarks"} />
+        ) : (
+          <div className='grid grid-cols-fluid gap-6 items-center'>
+            {myBookmarked?.length === 0 ? (
+              <div>
+                <h3 className='text-white text-sm'>No Bookmarks</h3>
+              </div>
+            ) : (
+              <div className='grid lg:grid-cols-4 md:grid-cols-2 max-sm:grid-cols-1  gap-6 items-center'>
+                {checkIFBookmarkEmpty?.map((movie: any) => (
+                  <div className='w-[250px] '>
+                    <Link href={`movies/${movie?.id}`}>
+                      <Image
+                        src={imagePath + movie?.poster_path}
+                        alt={`${movie?.title || ""}`}
+                        className='h-[350px] w-[250px] rounded-md bg-stone-300 transition ease-in-out cursor-pointer hover:brightness-50'
+                        loading='lazy'
+                        width={500}
+                        height={500}
+                        blurDataURL={imagePath + movie?.poster_path}
+                        placeholder='blur'
+                      />
+                    </Link>
+                    <div className='flex gap-2 relative -mt-[20em] float-right px-2'>
+                      <button
+                        title='bookmark movie'
+                        className={`text-xs bg-white text-slate-500 px-3 py-3 hover:scale-110 transition ease-in-out rounded-full`}
+                        onClick={() => handleRemoveBookmarked(movie?.id)}
+                      >
+                        <BookmarkAddedIcon className='text-green-500' />
+                      </button>
+                    </div>
+                    <h1 className='mt-3 text-slate-600 font-semibold tracking-tight'>
+                      {movie?.title}
+                    </h1>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        ))}
-      </div> */}
+        )}
+      </div>
     </div>
   );
 };
