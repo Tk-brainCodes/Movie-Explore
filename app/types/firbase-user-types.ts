60542b34fda80e1e068ab3dcaf3a3198b3f551c7
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-07-19 21:50:07 +0100

    feat(type): firebase authentication types

---
+++ app/types/firbase-user-types.ts
@@ -0,0 +1,18 @@
+export interface UserProps {
+  accessToken: string | any;
+  auth: any;
+  displayName: string;
+  email: string | any;
+  emailVerified: boolean;
+  isAnonymous: boolean;
+  metadata: any;
+  phoneNumber: any;
+  photoURL: string | any;
+  proactiveRefresh: any;
+  providerData: any;
+  providerId: string | any;
+  reloadListener: any | null;
+  reloadUserInfo: any;
+  tenantId: any | null;
+  uid: string;
+}
