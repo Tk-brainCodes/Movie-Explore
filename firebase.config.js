15d77ddd09504dda37ef452ed987ac29eec22ff0
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-07-28 00:46:15 +0100

    fix: fixed typo

--- firebase.config.js
+++ firebase.config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
