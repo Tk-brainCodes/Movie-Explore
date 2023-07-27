714267f50ca867ab7ec723eeafc622bd7de2c2b8
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-07-28 00:59:05 +0100

    fix: fixed next auth key on vercel

--- firebase.config.js
+++ firebase.config.js
@@ -1,3 +1,5 @@
+
+
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
@@ -16,4 +18,4 @@ const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 
-export default app 
+export default app
