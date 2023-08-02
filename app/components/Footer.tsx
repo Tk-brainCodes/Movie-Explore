f70e557ca09571551802448e3d96b17161f2e2c3
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-08-03 00:05:49 +0100

    feat: changed footer background, and fixed anchor link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,13 +2,12 @@ import { Anchor } from "./Anchor"
 
 const Footer = () => {
   return (
-    <footer className='bg-white rounded-lg shadow m-4 dark:bg-gray-800'>
-      <div className='w-[50vw] mx-auto max-w-screen-xl p-4 block'>
+    <div className='w-full h-20 border-t border-zinc-800 bg-[#121212]	px-8 py-4'>
         <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-          © 2023{" "}
-          <a href='#' className='hover:underline  cursor-pointer'>
-            Movie Explore™
-          </a>
+          © 2023
+          <Anchor href='https://movie-explore-seven.vercel.app/' target="_blank" >
+            <span className='hover:underline  cursor-pointer'>Movie Explore™</span>
+          </Anchor>
           . All Rights Reserved.
         </span>
         <div className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0'>
@@ -31,8 +30,7 @@ const Footer = () => {
             </Anchor>
           </p>
         </div>
-      </div>
-    </footer>
+    </div>
   );
 }
 export default Footer
