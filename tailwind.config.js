d761c85d2c53575104877c0e7b21349ba376472a
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-08-03 00:02:08 +0100

    feat(plugins): added plug to support bg-blur on firefox"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
@@ -18,5 +21,21 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+	plugin(function ({ addVariant, e, postcss }) {
+	  addVariant('firefox', ({ container, separator }) => {
+		const isFirefoxRule = postcss.atRule({
+		  name: '-moz-document',
+		  params: 'url-prefix()',
+		});
+		isFirefoxRule.append(container.nodes);
+		container.append(isFirefoxRule);
+		isFirefoxRule.walkRules((rule) => {
+		  rule.selector = `.${e(
+			`firefox${separator}${rule.selector.slice(1)}`
+		  )}`;
+		});
+	  });
+	}),
+  ]
 }
