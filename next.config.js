ebb1d92bdf4c8462e18f55c72f3e7c6e67615b4f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-09 13:21:38 +0100

    chore: downgraded to next 13

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
   images: {
     domains: ['image.tmdb.org', 'lh3.googleusercontent.com']
   },
